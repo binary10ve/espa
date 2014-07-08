1e41d5898c76009826293142632111a97b2ed44a
Sanil Naik <sanil.m.naik@gmail.com>
2014-07-08 23:57:38 +0530

    Logout page

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module Espa
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')  
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    config.assets.precompile += ['dessert.css']
+
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
